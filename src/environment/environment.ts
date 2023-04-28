a5515612f33fe7878b769b7269e2c2480590c910
Stian Haugland <stian.haugland@tietoevry.com>
2023-04-28 11:01:44 +0200

    fix vercel config

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const { AUTH0_ISSUER_BASE_URL } = process.env || "https://example-api.eu.auth0.com";
+export const { AUTH0_ISSUER_BASE_URL } =
+  process.env || "https://example-api.eu.auth0.com";
 export const { BASE_URL } = process.env || "http://localhost";
 
 if (!AUTH0_ISSUER_BASE_URL) {
