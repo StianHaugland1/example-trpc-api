8b261e066b77a933a713a4e8b27c374449e86bbf
Stian Haugland <stian.haugland@tietoevry.com>
2023-05-02 14:21:26 +0200

    Add default auth0 url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const { AUTH0_ISSUER_BASE_URL } = process.env || "https://example-api.eu.auth0.com";
-export const { BASE_URL } = process.env || "http://localhost";
+export const AUTH0_ISSUER_BASE_URL = process.env.AUTH0_ISSUER_BASE_URL || "https://example-api.eu.auth0.com";
+export const BASE_URL = process.env.BASE_URL || "http://localhost";
 
 if (!AUTH0_ISSUER_BASE_URL) {
   console.error("AUTH0_ISSUER_BASE_URL is missing");
